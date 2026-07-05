08d1479efd80ba8fc59d953917caff645f867de9
CIELO CRISTAL MEZA VIZCARRA <cmezav@unsa.edu.pe>
2026-07-05 02:31:36 -0500

    test(system): agregar helper para permisos por usuario
    se agrego un metodo reutilizable para asignar permisos segun el rol principal del usuario

--- tests/System/PermissionFlowTest.php
+++ tests/System/PermissionFlowTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\System;
 
+use BookStack\Entities\Models\Entity;
 use BookStack\Users\Models\User;
 use Tests\TestCase;
 
@@ -18,5 +19,19 @@ class PermissionFlowTest extends TestCase
         $this->admin = $this->users->admin();
         $this->editor = $this->users->editor();
         $this->viewer = $this->users->viewer();
+    }
+
+    protected function setPermissionsForUser(
+        Entity $entity,
+        User $user,
+        array $actions = []
+    ): void {
+        $role = $user->roles->first();
+
+        $this->permissions->setEntityPermissions(
+            $entity,
+            $actions,
+            [$role]
+        );
     }
 }
