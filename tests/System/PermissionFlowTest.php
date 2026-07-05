24cceac00f6224a912ad32a4a44020b8e4bdc111
CIELO CRISTAL MEZA VIZCARRA <cmezav@unsa.edu.pe>
2026-07-05 02:29:43 -0500

    test(system): preparar usuarios del flujo de permisos
    se agregaron los usuarios admin editor y viewer que usaran los casos st-02

--- tests/System/PermissionFlowTest.php
+++ tests/System/PermissionFlowTest.php
@@ -2,8 +2,21 @@
 
 namespace Tests\System;
 
+use BookStack\Users\Models\User;
 use Tests\TestCase;
 
 class PermissionFlowTest extends TestCase
 {
+    protected User $admin;
+    protected User $editor;
+    protected User $viewer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->admin = $this->users->admin();
+        $this->editor = $this->users->editor();
+        $this->viewer = $this->users->viewer();
+    }
 }
