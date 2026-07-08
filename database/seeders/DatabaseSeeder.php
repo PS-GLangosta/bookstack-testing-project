99e7bce03ba6703d572a25710441b9e7cf09bc19
MAURICIO <mzegarrapu@unsa.edu.pe>
2026-07-08 16:52:17 -0500

    correcciones 5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        $this->call(UserRoleSeeder::class);
+        $this->call(DummyContentSeeder::class);
 
         Model::reguard();
     }
